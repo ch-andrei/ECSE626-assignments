826d5fa068552292bc0a1fd986a56b3b06d2aa20
Andrei Chubarau <andrei.chubarau@irystec.com>
2018-03-19 20:57:06 -0400

    final code

--- a2/gmm.py
+++ a2/gmm.py
@@ -15,11 +15,11 @@ imgName2 = "260058.jpg"
 
 # GMM segmentation configuration
 img2read = imgName0
-rescale = 1
+rescale = 0.25
 gmmK = 2
-gmmMaxIterations = 1000 # termination if epsilon criterion isn't reached
-gmmFeatureType = "lab"
-epsilon = 1e-2 # for convergence
+gmmMaxIterations = 250 # termination if epsilon criterion isn't reached
+gmmFeatureType = "i"
+epsilon = 1e-3 # for convergence
 epsilonP = 1e-6 # for precision
 
 # text dumps for silent logging instead of stdout; used when doVerbose is False
@@ -59,6 +59,9 @@ def img2LabArray(img):
 
 def imshow(imgname, img):
     cv2.imshow(imgname, img.astype(np.uint8))
+
+def checkNan(val):
+    return np.isnan(val) or np.isinf(val) or np.isneginf(val)
 
 def readAndPreprocessImage(imgname=img2read,
                            featureType=gmmFeatureType
@@ -135,8 +138,8 @@ class GMM:
         self.means = None
         self.covars = None
         self.w = None
-        self.loglikelihoods = None
-        self.bics = None
+        self.loglikelihood = None
+        self.bic = None
 
     def P(self, x, means, covars, d, k):
         xmu = x - means[k]
@@ -153,6 +156,11 @@ class GMM:
             raise Exception("GMM: Attempting to predict using untrained GMM.")
 
         n, d = x.shape[:2] # number of samples and dimensionality of each sample
+
+        # rescale to full range
+        x -= x.min() - epsilonP # epsilon to avoid 0 and 1 values
+        x /= x.max() + epsilonP # epsilon to avoid 0 and 1 values
+
         w = np.zeros((n, self.k), np.float32) # membership weight of each data point to each Gaussian
 
         # compute raw probabilities
@@ -188,8 +196,8 @@ class GMM:
         x -= x.min() - epsilonP # add epsilon to avoid 0 and 1 values
         x /= x.max() + epsilonP # add epsilon to avoid 0 and 1 values
 
-        alphas = np.array([1.0 / self.k for i in range(self.k)], np.float32) # alphas for each Gaussian
-        covars = np.array([np.eye(d) if d > 1 else [[1]] for i in range(self.k)], np.float32) # identity covar matrices for each Gaussian
+        alphas = np.array([1.0 / self.k for _ in range(self.k)], np.float) # alphas for each Gaussian
+        covars = np.array([np.eye(d) if d > 1 else [[1]] for _ in range(self.k)], np.float) # identity covar matrices for each Gaussian
         means = x[(np.random.rand(self.k) * n).astype(np.int)] # pick random means for each Gaussian
 
         w = np.zeros((n, self.k), np.float32) # membership weight of each data point to each Gaussian
@@ -276,9 +284,7 @@ class GMM:
             doprint("means updated", means)
             doprint("covars updated", covars)
 
-            if np.isnan(loglikelihood) \
-                    or np.isinf(loglikelihood) \
-                    or np.isneginf(loglikelihood):
+            if checkNan(loglikelihood):
                 if not doVerbose and dumgLogsOnNan:
                     dumpLogs()
                 break
@@ -293,8 +299,8 @@ class GMM:
         self.means = means
         self.covars = covars
         self.w = w
-        self.loglikelihoods = loglikelihoods
-        self.bics = bics
+        self.loglikelihood = loglikelihoods[-1]
+        self.bic = bics[-1]
         self.trained = True
 
 def doKFoldCrossValidation(imgname=img2read,
@@ -304,10 +310,12 @@ def doKFoldCrossValidation(imgname=img2read,
                            ):
     img, x = readAndPreprocessImage(imgname=imgname, featureType=featureType)
 
-    logkey = "Log Likelihood"
-    bickey = "Bayesian Information Criterion"
+    ks = []
+    logkey_train = []
+    logkey_test = []
+    bickey_train = []
+    bickey_test = []
 
-    results = {}
     for k_gmm in range(2, maxK+1):
         xshuf = x.copy()
         np.random.shuffle(xshuf) # randomize ordering of the vector X
@@ -319,7 +327,11 @@ def doKFoldCrossValidation(imgname=img2read,
 
         gmm = GMM(k_gmm)
 
-        crt = []
+        logl_train_avg = 0
+        logl_test_avg = 0
+        bic_train_avg = 0
+        bic_test_avg = 0
+
         for k_crossVal in range(k_crossValidation):
             print("\nRunning crossval with {} Gaussians on set {}".format(k_gmm, k_crossVal))
 
@@ -332,58 +344,71 @@ def doKFoldCrossValidation(imgname=img2read,
 
             # fit on train data
             gmm.fit(xt)
+            logl_train_avg +=  gmm.loglikelihood
+            bic_train_avg += gmm.bic
 
             # predict on test data
-            w, logl, bic = gmm.predict(yt)
-
-            crt.append({logkey: logl, bickey: bic})
-
-        def checkNan(val):
-            return np.isnan(val) or np.isinf(val) or np.isneginf(val)
+            _, logl_test, bic_test = gmm.predict(yt)
+            logl_test_avg += logl_test
+            bic_test_avg += bic_test
 
         # average cross validation results
-        logl = 0
-        bic = 0
-        count = [0, 0]
-        for values in crt:
-            if not checkNan(values[logkey]):
-                count[0] += 1
-                logl += values[logkey]
-            if not checkNan(values[bickey]):
-                count[1] == 1
-                bic += values[bickey]
+        logl_train_avg /= k_crossValidation
+        logl_test_avg /= k_crossValidation
+        bic_train_avg /= k_crossValidation
+        bic_test_avg /= k_crossValidation
 
-        logl /= count[0] + epsilonP # epsilon to avoid division by zero in rare failure cases
-        bic  /= count[1] + epsilonP # epsilon to avoid division by zero in rare failure cases
-
-        results[k_gmm] = {logkey:logl, bickey:-bic}
+        # put into lists for plotting
+        ks.append(k_gmm)
+        logkey_train.append(logl_train_avg)
+        logkey_test.append(logl_test_avg)
+        bickey_train.append(-bic_train_avg)
+        bickey_test.append(-bic_test_avg)
 
     # plot the results
-    ks = []
-    logs = []
-    bics = []
-    for k_gmm in results:
-        ks.append(k_gmm)
-        logs.append(results[k_gmm][logkey])
-        bics.append(results[k_gmm][bickey])
 
+    # PLOT TRAIN
     fig, ax1 = plt.subplots()
 
     color = 'tab:red'
     ax1.set_xlabel('Number of Gaussians')
-    ax1.set_ylabel(logkey, color=color)
-    ax1.plot(ks, logs, 'o-', color=color)
+    ax1.set_ylabel("Log Likelihood", color=color)
+    ax1.plot(ks, logkey_train, 'o-', color=color)
+
     ax1.tick_params(axis='y', labelcolor=color)
 
     ax2 = ax1.twinx()
 
     color = 'tab:blue'
-    ax2.set_ylabel(bickey, color=color)
-    ax2.plot(ks, bics, 'o-', color=color)
+    ax2.set_ylabel("Bayesian Information Criterion", color=color)
+    ax2.plot(ks, bickey_train, 'o-', color=color)
+
     ax2.tick_params(axis='y', labelcolor=color)
 
     fig.tight_layout()
     plt.show()
+
+    # PLOT TEST
+    fig, ax1 = plt.subplots()
+
+    color = 'tab:red'
+    ax1.set_xlabel('Number of Gaussians')
+    ax1.set_ylabel("Log Likelihood", color=color)
+    ax1.plot(ks, logkey_test, 'o-', color=color)
+
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    ax2 = ax1.twinx()
+
+    color = 'tab:blue'
+    ax2.set_ylabel("Bayesian Information Criterion", color=color)
+    ax2.plot(ks, bickey_test, 'o-', color=color)
+
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    fig.tight_layout()
+    plt.show()
+
 
 def doGMMsegmentation(imgname=img2read,
                       k=gmmK,
@@ -429,7 +454,7 @@ def main():
 
     # do segmentation and save images
     # for k in range(2, 9, 2):
-    #     doGMMsegmentation(k=k)
+    #     doGMMsegmentation(k=2)
 
 
 if __name__=="__main__":
